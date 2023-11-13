aaa7332c31f28e59b68e00013a00e778f3bb475c
ahmad oa <ouladaouida@gmail.com>
2023-11-13 18:47:19 +0100

    updated scroll to auto

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -220,7 +220,7 @@ export default function Navbar() {
               </button>
             </PopoverTrigger>
             <PopoverContent
-              className={`h-80 max-h-80 flex flex-col gap-3 p-3 overflow-y-scroll ${
+              className={`h-80 max-h-80 flex flex-col gap-3 p-3 overflow-y-auto${
                 notifications.length === 0 && "justify-center items-center"
               }}`}
               align="end"
